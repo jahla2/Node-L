36c8246cfb89cbbcbbc34da824940363b154e7d3
Reynand Ocso <73638224+jahla2@users.noreply.github.com>
2022-12-18 21:42:04 +0800

    Node
    Learning Node

---
+++ intro-to-node/index.js
@@ -0,0 +1,13 @@
+//INternal Node js module
+// const filesystem =  require("fs");
+// filesystem.copyFileSync("file1.txt","file2.txt");
+
+//Super hero package
+// var superheroes = require('superheroes');
+// var mysuperheroname = superheroes.random();
+// console.log(mysuperheroname);
+
+//Super supervillains package
+var supervillains = require('supervillains');
+var mysupervillains = supervillains.all;
+console.log(mysupervillains);
